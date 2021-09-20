ff33bb4cd2a3a2d54ef026c3a155f7d24866d361
Leia <87383466+klynmc@users.noreply.github.com>
2021-09-19 20:59:58 -0500

    add buttons

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
-const d = new Date();
-document.getElementById("currentDay").innerHTML = d;
+const currentDay = new Date();
+document.getElementById("currentDay").innerHTML = currentDay;
+
+var auditTask = function(taskEl) {
+    //get date from task element
+    var date = $(taskEl)
+    .find("span")
+    .text()
+    .trim();
+  
+    //convert to moment object at 5:00pm
+    var time = moment(date, "L").set("hour", 17);
+    
+    //remove any old classes from element
+    $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+  
+    //apply new class if task is near/over due date
+    if (moment().isAfter(time)) {
+      $(taskEl).addClass("list-group-item-danger");
+    } else if (Math.abs(moment().diff(time, "days")) <= 2) {
+      $(taskEl).addClass("list-group-item-warning");
+    }
+  };
+
+  setInterval(function() {
+    $(".card .list-group-item").each(function() {
+      auditTask($(this));
+    });
+  }, 1800000);
+  
